bd12c795d8a75485a0d541a1c16bb8d84e3b1320
jayli3n <jayli3n@gmail.com>
2024-10-25 16:17:24 +0800

    Able to move down one way

--- Game/Scripts/character_body_2d.gd
+++ Game/Scripts/character_body_2d.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 150.0
-const JUMP_VELOCITY = -450.0
+const JUMP_VELOCITY = -350.0
 const dEd = 90.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -20,6 +20,9 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x =  move_toward(velocity.x, 0, SPEED)
+	
+	if Input.is_action_just_pressed("Down") and is_on_floor():
+		position.y += 3
 
 	move_and_slide()
 
